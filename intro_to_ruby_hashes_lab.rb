0674a7f08397e45a27a1165d6dc6ad93fde5025a
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-14 23:38:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,8 +4,8 @@ def base_hash
 end
 
 def monopoly_with_second_tier
- base_hash = {:pieces = 4}
-base_hash[:railroads] = [:pieces = 4]
+base_hash[:railroads] = { name: "Prince Escalus" }
+
 end
 
 def monopoly_with_third_tier
