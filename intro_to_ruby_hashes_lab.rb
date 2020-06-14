1129b91f4a45699575ef36ac7d83de6ebb40dc43
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-14 23:49:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,7 +6,7 @@ end
 def monopoly_with_second_tier
 base_hash {
  :railroads => [
-            {:pieces => "4"},
+            {:pieces => 4},
             {:rent_in_dollars => {} },
             {:names => {} }
 ]
