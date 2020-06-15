bf2c00275a6734deef7a1ddc795cfbf84c38a367
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-15 00:33:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ monopoly_with_second_tier = {
             :one_piece_owned => 25,
             :two_pieces_owned => 50,
             :three_pieces_owned => 100,
-            
+            :four_pieces_owned => 200
               
             },
             :names => {} 
