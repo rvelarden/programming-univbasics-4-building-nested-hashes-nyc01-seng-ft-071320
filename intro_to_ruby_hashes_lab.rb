8b1a0084c1d5507e5b78c119f5574134f8a384a1
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-15 00:38:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -53,7 +53,9 @@ monopoly_with_third_tier = {
             },
             :names => {
               
-            :reading_railroad => {},
+            :reading_railroad => {
+            :mortgage_value => 100,
+            },
             :pennsylvania_railroad => {},
             :b_and_o_railroad => {},
             :shortline_railroad => {}
