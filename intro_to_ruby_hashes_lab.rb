cd3e055cc700642ee5ae76d30a483b6242067d29
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-15 00:28:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,11 +14,7 @@ base_hash = {
 end
 
 def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
-base_hash = {
+monopoly_with_second_tier = {
  :railroads => {
             :pieces => 4,
             :rent_in_dollars => {},
