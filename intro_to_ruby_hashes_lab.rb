6b886e78922b533da0837165bbe85fae213686fc
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-15 00:35:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,6 +29,7 @@ monopoly_with_second_tier = {
               
             :reading_railroad => {},
             :pennsylvania_railroad => {},
+            :b_and_o_railroad => {},
             
               
             } 
